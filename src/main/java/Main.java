23ee502a4957410cfe94a0d8bb90511ae497ed34
uiop860 <github+olivertaehr96@gmail.com>
2021-02-12 19:51:03 +0100

    Comments out bad tests in main class

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,6 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
 
+        /*
         Customer c1 = new Customer("Jon");
         Account account = new Account(c1);
 
@@ -12,6 +13,7 @@ public class Main {
 
         newBalance += account.depositAmount(325);
         System.out.println(String.format("Ny balance: %d", newBalance));
+        */
 
     }
 }
