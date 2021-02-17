5a1821a38379674d50475ef402d45595b2822504
Sebastian Hejlesen <Cph-sh528@cphbusiness.dk>
2021-02-17 12:43:52 +0100

    UI update //Withdraw method is almost working

--- src/main/java/UI.java
+++ src/main/java/UI.java
@@ -1,30 +1,50 @@
+import java.sql.SQLOutput;
 import java.util.Scanner;
 
 public class UI {
 
     private static Customer customer;
     private static Account account;
-   ;
+
+    ;
 
 
-        public String takeStringInput(){
-            Scanner input = new Scanner(System.in);
-            return  input.nextLine();
-        }
+    public String takeStringInput() {
+        Scanner input = new Scanner(System.in);
+        return input.nextLine();
+    }
 
+    int id = 0;
 
-    public void run() {
+    public void Setup() {
         System.out.println("Velkommen til banken");
-        System.out.println("1 - Hæve penge \n 2 - Indsæt penge\n 3 - Kontoudtog\n 4 - Overfør penge\"");
+        System.out.println("Indtast venligst dit kunde ID");
+        try {
+            id = Integer.parseInt(takeStringInput());
+            System.out.println("Mange tak, vælg venligst et af følgende funktioner");
+        } catch (NullPointerException e) {
+            System.out.println(e);
+        }
+    }
+        public void run(){
+        System.out.println("1 - Hæve penge \n 2 - Indsæt penge\n 3 - Kontoudtog\n 4 - Overfør penge");
         String input = takeStringInput();
         switch (input) {
             case "1":
-                System.out.println("Indtast venligst dit konto nummer");
-                int id = Integer.parseInt(takeStringInput());
-                int amount = account.getBalance(id);
-                System.out.println("Du har:" + amount);
-                System.out.println("Hvor mange penge vil du hæve?");
+                int currentBalance = Dao.checkBalance(id);
+                System.out.println("Du har:" + currentBalance);
+                try {
+                    System.out.println("Hvor mange penge vil du hæve?");
+                    int withdrawAmount = Integer.parseInt(takeStringInput());
+                    //sidste ID skal ændres i nedenstående sætning//
+                    Dao.makeTransaction(withdrawAmount, id, id);
+                    int currentBalanceAfterWithdraw = Dao.checkBalance(id);
+                    System.out.println("Du har:" + currentBalanceAfterWithdraw);
+                    run();
+                } catch (NullPointerException e) {
 
+                    System.out.println(e);
+                }
 
                 break;
             case "2":
@@ -38,9 +58,10 @@ public class UI {
             case "4":
                 System.out.println("Vælg beløb til");
             default:
-                System.out.println("forkert input, prøv igen");
+                System.out.println("Forkert input, prøv igen");
 
 
         }
 
-    }}
+    }
+}
